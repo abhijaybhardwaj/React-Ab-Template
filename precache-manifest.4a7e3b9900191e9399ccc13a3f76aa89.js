6a55e89fac61d7b67a6877603d214c40387f6b18
abhijaybhardwaj <asbfreelancing@gmail.com>
2019-08-22 16:46:10 +0530

    Updates

---
+++ precache-manifest.4a7e3b9900191e9399ccc13a3f76aa89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0aef0109a14d56895b0031f3ceb8e6",
+    "url": "/React-Ab-Template/index.html"
+  },
+  {
+    "revision": "9bbdbd1b71af53c7a82e",
+    "url": "/React-Ab-Template/static/css/main.5e8f8ea5.chunk.css"
+  },
+  {
+    "revision": "7a94b09931f12e07e3b3",
+    "url": "/React-Ab-Template/static/js/2.b03b4605.chunk.js"
+  },
+  {
+    "revision": "9bbdbd1b71af53c7a82e",
+    "url": "/React-Ab-Template/static/js/main.6685de89.chunk.js"
+  },
+  {
+    "revision": "cc12c5748a6b9b6b1de2",
+    "url": "/React-Ab-Template/static/js/runtime~main.b8019bed.js"
+  }
+]);
